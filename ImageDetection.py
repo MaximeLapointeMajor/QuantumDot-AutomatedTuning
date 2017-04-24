b5c17fde852e006882921bc6f6df1ccffd0900b9
MaximeLapointeMajor <maxime.lapointe-major@usherbrooke.ca>
2017-04-24 11:18:31 -0400

    Add files via upload
    Added an Initialization() function that builds the list of splitted clusters (used to capture the curvature found on some transitions) and the _split() function.  Also added the Transition class that will be used as a wraper for a list of clusters all part of a same transition

--- ImageDetection.py
+++ ImageDetection.py
@@ -9,7 +9,7 @@ import numpy as np
 #from skimage import measure
 #import matplotlib.pyplot as plt
 #import matplotlib.cm as cm
-from itertools import product
+from itertools import product, izip
 from numpy.linalg import eig
 import lmfit as lmf
 
@@ -56,6 +56,7 @@ class segment:
 
 class Cluster:
     """
+    
     """
     def __init__(self, cluster, max_gap):
         
@@ -113,13 +114,19 @@ class Cluster:
         correction = nPoints*ratio/(1.+max_gap)
         cp = _rotate(cluster, u, v, p0)
 
+        corr_sigma_theta = np.sqrt(sigma_theta_sq)*correction
+        if corr_sigma_theta > np.pi/2:
+            corr_sigma_theta= np.pi/2
+
         self.correl_coeff = correl_coeff
         self.correction_coeff = correction
-        self.corr_sigma_theta = np.sqrt(sigma_theta_sq)*correction
+        self.corr_sigma_theta = corr_sigma_theta
         self.corr_sigma_rho = np.sqrt(sigma_rho_sq)*correction
         self.corr_sigma_rt = sigma_rho_theta*correction*correction
         self.corr_x = cp[1]
         self.corr_y = cp[0]
+        
+
 
         start, stop = _start_stop(cp[1], cp[0], u, v, p0)
         self.start = start
@@ -147,85 +154,90 @@ class Cluster:
         self.xSegment = xSegment
         self.ySegment = ySegment
 
-
-def _segment_start_stop(x, y, a, b):
+class Transition:
     """
+    
     """
-    if a > 0.:
-        xmax = max(x)
-        xmin = min(x)
-        ymax = max(y)
-        ymin = min(y)
-        xmax_y = a*xmax+b
-        xmin_y = a*xmin+b
-        if xmax == xmin:
-            pass
+    
+    def __init__(self, cluster, transition=None):
+        
+        clist = []
+        if transition == None:
+            clist.append(cluster)
         else:
-            if xmin_y < ymin:
-                xmin = (ymin-b)/a
-                #ymin est bon et ymin_x est le x associé.
-            elif ymin <= xmin_y:
-                ymin = xmin_y.copy()
-                #xmin_y est bon et xmin est le x associé.
-            if xmax_y < ymax:
-                ymax = xmax_y.copy()
-                #xmax_y est bon est xmax est le x associé
-            elif ymax <= xmax_y:
-                xmax = (ymax-b)/a
-                #ymax est bon et ymax_x est le x associé
-    elif a < 0.:
-        xmax = max(x)
-        xmin = min(x)
-        ymax = min(y)
-        ymin = max(y)
-        xmax_y = a*xmax+b
-        xmin_y = a*xmin+b
-        if xmax == xmin:
-            pass
-        else:
-            if xmin_y > ymin:
-                xmin = (ymin-b)/a
-                #ymin est bon et ymin_x est le x associé.
-            elif ymin >= xmin_y:
-                ymin = xmin_y.copy()
-                #xmin_y est bon et xmin est le x associé.
-            if xmax_y > ymax:
-                ymax = xmax_y.copy()
-                #xmax_y est bon est xmax est le x associé
-            elif ymax >= xmax_y:
-                xmax = (ymax-b)/a
-                #ymax est bon et ymax_x est le x associé
-    elif a == 0.:
-        xmax = max(x)
-        xmin = min(x)
-        ymax = a*xmax+b
-        ymin = ymax.copy()
-    return np.array((ymin, xmin)), np.array((ymax, xmax))
+            dist0 = (cluster.p0_x-transition.sStart_x)*(cluster.p0_x-transition.sStart_x)+(cluster.p0_y-transition.sStart_y)*(cluster.p0_y-transition.sStart_y)
+            dist1 = (cluster.p0_x-transition.sStop_x)*(cluster.p0_x-transition.sStop_x)+(cluster.p0_y-transition.sStop_y)*(cluster.p0_y-transition.sStop_y)
+            if dist0 < dist1:
+                clist.append(cluster)
+                for u in transition.clusters:
+                    clist.append(u)
+            else:
+                for u in transition.clusters:
+                    clist.append(u)
+                clist.append(cluster)
+        
+        self.clusters = clist
+        
+        p0 = []
+        p0_x = []
+        p0_y = []
+        for u in clist:
+            p0.append(u.p0)
+            p0_x.append(u.p0_x)
+            p0_y.append(u.p0_y)
+        
+        self.p0s = p0
+        self.p0_xs = p0_x
+        self.p0_ys = p0_y
+        
+        self.sStart = clist[0].seg_start
+        self.sStart_x = clist[0].seg_start_x
+        self.sStart_y = clist[0].seg_start_y
+        self.sStop = clist[-1].seg_stop
+        self.sStop_x = clist[-1].seg_stop_x
+        self.sStop_y = clist[-1].seg_stop_y
+        
+        xSegment = []
+        ySegment = []
+        
+        xSegment.append(clist[0].seg_start_x)
+        for u in p0_x:
+            xSegment.append(u)
+        xSegment.append(clist[-1].seg_stop_x)
+        
+        ySegment.append(clist[0].seg_start_y)
+        for u in p0_y:
+            ySegment.append(u)
+        ySegment.append(clist[-1].seg_stop_y)
+        
+        self.xSegment = xSegment
+        self.ySegment = ySegment
+        
+        self.cStart = clist[0]
+        self.cStop = clist[-1]
+        
+        
         
 
 
 
+def Initialization(cc, max_gap=0):
+    cc = list(cc)
+    leftover = cc.pop(-1)
+    clust = []
+    for u in cc:
+        temp = Cluster(u, max_gap)
+        ret = _split(temp.corr_x, temp.corr_y, temp.u, temp.v, temp.p0, temp.ratio)
+        if ret == None:
+            clust.append(temp)
+        else:
+            cc.append(ret[0])
+            cc.append(ret[1])
+    clust.append(Cluster(leftover, max_gap))
+    return clust
 
 
-def _start_stop(corrx, corry, u, v, p0):
-    """
-    """
-    index = np.argsort(corrx)
-    x0 = corrx[index[0]]
-    x1 = corrx[index[-1]]
-    y0 = corry[index[0]]
-    y1 = corry[index[-1]]
-    xstart = int(round(u[1]*x0-u[0]*y0+p0[1]))
-    ystart = int(round(-v[1]*x0+v[0]*y0+p0[0]))
-    xstop = int(round(u[1]*x1-u[0]*y1+p0[1]))
-    ystop = int(round(-v[1]*x1+v[0]*y1+p0[0]))
-    if ystart < ystop:
-        start = np.array((ystart, xstart))
-        stop = np.array((ystop, xstop))
-    else:
-        stop = np.array((ystart, xstart))
-        start = np.array((ystop, xstop))
-    return start, stop
+
 
 
 def DistancePointLine(x, y, a, b):
@@ -290,7 +302,13 @@ def Linkage(image, gap_size=0, min_cluster_size=4):
 
 def _group(img, ref, gap_size=0):
     """
-    
+    The _group function takes a binary image and a reference coordinate and generates an array of all the points in the image that are part of the same cluster as the reference point.
+
+    The function returns an array of coordinates that all belong to a single cluster.
+
+    img is a binary image where all non-zero values are pixels that belong to a cluster.
+    ref is a coordinate of the reference point for which the _group function has to find all other points belonging to the same cluster.
+    gap_size is the maximum size of gaps between pixels (or groups of pixels) for them to be considered as part of a same cluster.
     """
     index = 0
     mylist = []
@@ -308,7 +326,11 @@ def _group(img, ref, gap_size=0):
 
 def _next(img, ref, gap_size=0):
     """
+    The _next function takes a binary image, a reference point and a gap_size as an input.
     
+    The coordinate of the first non-zero point found within the gap_size of the reference point is returned.
+
+    If none of the points around the reference are non-zero, the function returns None.
     """
     ret = None
     for i in product((np.arange(ref[0]-gap_size-1, ref[0]+gap_size+2, 1))%img.shape[0], (np.arange(ref[1]-gap_size-1, ref[1]+gap_size+2, 1))%img.shape[1]):
@@ -317,36 +339,101 @@ def _next(img, ref, gap_size=0):
             break
     return ret
 
-def Segmentation(cluster):
-    p0, u ,v = u_v_(cluster)
-    cp = _rotate(cluster, u, v, p0)
-    index = np.argsort(cp[1])
-    length, ratio = _ratio(cp)
-    i = np.where(abs(cp[0, index])==max(abs(cp[0, index])))[0][0]
-    cp0 = cp[:,i:]
-    cp1 = cp[:,:i]
-    if cp0.shape[1]<5 or cp1.shape[1]<5:
-        ret = None
-    else:
-        clow = cluster.T[:,index[:i]].T
-        chigh = cluster.T[:,index[i:]].T
-        p0l, ul ,vl = u_v_(clow)
-        p0h, uh ,vh = u_v_(chigh)
-        cpl = _rotate(clow, ul, vl, p0l)
-        cph = _rotate(chigh, uh, vh, p0h)
-        lengthl, ratiol = _ratio(cpl)
-        lengthh, ratioh = _ratio(cph)
-        if ratiol>1.:
-            ratiol=1./ratiol
-        if ratioh>1.:
-            ratioh=1./ratioh
-        if ratiol<ratio or ratioh<ratio:
-            ret = clow, chigh
+def _segment_start_stop(x, y, a, b):
+    """
+    
+    """
+    if a > 0.:
+        xmax = max(x)
+        xmin = min(x)
+        ymax = max(y)
+        ymin = min(y)
+        xmax_y = a*xmax+b
+        xmin_y = a*xmin+b
+        if xmax == xmin:
+            pass
         else:
-            ret = None
-    return ret
+            if xmin_y < ymin:
+                xmin = (ymin-b)/a
+                #ymin est bon et ymin_x est le x associé.
+            elif ymin <= xmin_y:
+                ymin = xmin_y.copy()
+                #xmin_y est bon et xmin est le x associé.
+            if xmax_y < ymax:
+                ymax = xmax_y.copy()
+                #xmax_y est bon est xmax est le x associé
+            elif ymax <= xmax_y:
+                xmax = (ymax-b)/a
+                #ymax est bon et ymax_x est le x associé
+    elif a < 0.:
+        xmax = max(x)
+        xmin = min(x)
+        ymax = min(y)
+        ymin = max(y)
+        xmax_y = a*xmax+b
+        xmin_y = a*xmin+b
+        if xmax == xmin:
+            pass
+        else:
+            if xmin_y > ymin:
+                xmin = (ymin-b)/a
+                #ymin est bon et ymin_x est le x associé.
+            elif ymin >= xmin_y:
+                ymin = xmin_y.copy()
+                #xmin_y est bon et xmin est le x associé.
+            if xmax_y > ymax:
+                ymax = xmax_y.copy()
+                #xmax_y est bon est xmax est le x associé
+            elif ymax >= xmax_y:
+                xmax = (ymax-b)/a
+                #ymax est bon et ymax_x est le x associé
+    elif a == 0.:
+        xmax = max(x)
+        xmin = min(x)
+        ymax = a*xmax+b
+        ymin = ymax.copy()
+    return np.array((ymin, xmin)), np.array((ymax, xmax))
+
+def _backrotate(cc, u, v, p0):
+    cpx = np.zeros(cc[0].shape[0], dtype=np.int32)
+    cpy = np.zeros(cc[0].shape[0], dtype=np.int32)
+    index = np.linspace(0, cc[0].shape[0]-1, cc[0].shape[0], dtype=np.int32)
+    for i, j, k in izip(cc[0], cc[1], index):
+        cpx[k] = int(round(u[1]*j-u[0]*i+p0[1]))
+        cpy[k] = int(round(-v[1]*j+v[0]*i+p0[0]))
+    cp = np.array((cpy, cpx))
+    return cp.T
+
+def _start_stop(corrx, corry, u, v, p0):
+    """
+    
+    """
+    index = np.argsort(corrx)
+    x0 = corrx[index[0]]
+    x1 = corrx[index[-1]]
+    y0 = corry[index[0]]
+    y1 = corry[index[-1]]
+    xstart = int(round(u[1]*x0-u[0]*y0+p0[1]))
+    ystart = int(round(-v[1]*x0+v[0]*y0+p0[0]))
+    xstop = int(round(u[1]*x1-u[0]*y1+p0[1]))
+    ystop = int(round(-v[1]*x1+v[0]*y1+p0[0]))
+    if ystart < ystop:
+        start = np.array((ystart, xstart))
+        stop = np.array((ystop, xstop))
+    else:
+        stop = np.array((ystart, xstart))
+        start = np.array((ystop, xstop))
+    return start, stop
 
 def _rotate(cluster, u, v, p0):
+    """
+    The _rotate function takes a cluster of points and its u, v and p0 vectors and makes a translation (of -p0) followed by a rotation (of pi/2-arccos(v_x)) of the cluster.
+
+    cluster is an array of the coordinates that need to be translated and rotated.
+    u is the vector along which the x axis will be after the rotation.
+    v is the vector along which the y axis will be after the rotation.
+    -p0 is the vector of translation that is applied prior to the rotation.  p0 is typically the middle of the cluster that needs to be rotated.
+    """
     clust = cluster.copy()
     clust = clust.T
     cp = np.zeros(clust.shape)
@@ -389,6 +476,10 @@ def u_v_(cluster):
     return p0, u, v
 
 def _correction_uv(cluster, u, v, p0):
+    """
+    The _correction_uv function takes a cluster of points and its u, v and p0 vectors, applies the translation followed by rotation specified by these 3 vectors using the _rotate() function before applying a linear fit to the rotated set of data.
+    The slope of the fit is extracted and the angle of rotation (so the u and v vectors) are corrected so that when applied, the slope of the cluster is zero.
+    """
     cp = _rotate(cluster, u, v, p0)
     index = np.argsort(cp[1])
     mod = lmf.models.LinearModel()
@@ -408,6 +499,9 @@ def _correction_uv(cluster, u, v, p0):
     return p0, u, v#, ratio, length, covar
 
 def _ratio(cp):
+    """
+    
+    """
     length = max(cp[1])-min(cp[1])
     ratio = (max(cp[0])-min(cp[0])+1)/length
     return length, ratio
@@ -477,21 +571,125 @@ def Sigmas(cluster, p0, u, v):
     sigma_rho_sq, sigma_theta_sq, sigma_rho_theta = _sigmas(p0, u, v, sigma_mp_sq, sigma_bp_sq)
     return sigma_rho_sq, sigma_theta_sq, sigma_rho_theta
 
+def _split(corrx, corry, u, v, p0, ratio):
+    """
+    corrx is an array of the x axis coordinates of all the points part of the cluster.
+    corry is an array of the y axis coordinates of all the points part of the cluster.
+    sPoint is the splitting x-coordinate. If not specified, sPoint=None and will be taken as the maximum distance along the y axis (max(abs(corry)))
+    
+    
+    """
+    index = np.argsort(corrx)
+    a = (corry[index][-1]-corry[index][0])/(corrx[index][-1]-corrx[index][0])
+    b = corry[index][0]-a*corrx[index][0]
+    d = []
+    for i, j in izip(corrx, corry):
+        d.append(DistancePointLine(i, j, a, b))
+    d = np.array(d)
+    ind = np.where(d==max(d))
+    ind = ind[0][0]
+    c0 = np.array((corry[index][:ind], corrx[index][:ind]))
+    c1 = np.array((corry[index][ind:], corrx[index][ind:]))
+    if c0.shape[1]>4 and c1.shape[1]>4:
+        p00, u0, v0 = u_v_(c0.T)
+        if v0[0]<0.:
+            v0 = -v0
+        if u0[1]<0.:
+            u0 = -u0
+        p00, u0, v0 = _correction_uv(c0.T, u0, v0, p00)
+        if v0[0]<0.:
+            v0 = -v0
+        if u0[1]<0.:
+            u0 = -u0
+        cp0 = _rotate(c0.T, u0, v0, p00)
+        length0, ratio0 = _ratio(cp0)
+        if ratio >= ratio0:
+            c0 = _backrotate(c0, u, v, p0)
+            c1 = _backrotate(c1, u, v, p0)
+            ret = c0, c1
+        elif ratio < ratio0:
+            p01, u1, v1 = u_v_(c1.T)
+            if v1[0]<0.:
+                v1 = -v1
+            if u1[1]<0.:
+                u1 = -u1
+            p01, u1, v1 = _correction_uv(c1.T, u1, v1, p01)
+            if v1[0]<0.:
+                v1 = -v1
+            if u1[1]<0.:
+                u1 = -u1
+            cp1 = _rotate(c1.T, u1, v1, p01)
+            length1, ratio1 = _ratio(cp1)
+            if ratio >= ratio1:
+                c0 = _backrotate(c0, u, v, p0)
+                c1 = _backrotate(c1, u, v, p0)
+                ret = c0, c1
+            else:
+                ret = None
+    else:
+        ret = None
+    return ret
 
 
-def _kernel(angle, dist, sigma_rho, sigma_theta, sigma_rho_theta, rho, theta_y):
-    angle = angle*np.pi/180.
-    r = sigma_rho_theta/(sigma_rho*sigma_theta)
-    kernel = np.zeros((dist.shape[0],angle.shape[0]))
-    for u, i in enumerate(angle):
-        for j, k in enumerate(dist):
-            z0 = (k-rho)*(k-rho)/(sigma_rho*sigma_rho)-2*r*(k-rho)*(i-theta_y)/(sigma_rho*sigma_theta)+(i-theta_y)*(i-theta_y)/(sigma_theta*sigma_theta)
-#            z1 = (k+rho)*(k+rho)/(sigma_rho*sigma_rho)-2*r*(k+rho)*(i-theta_y-np.pi)/(sigma_rho*sigma_theta)+(i-theta_y-np.pi)*(i-theta_y-np.pi)/(sigma_theta*sigma_theta)
-#            z2 = (k+rho)*(k+rho)/(sigma_rho*sigma_rho)-2*r*(k+rho)*(i-theta_y+np.pi)/(sigma_rho*sigma_theta)+(i-theta_y+np.pi)*(i-theta_y+np.pi)/(sigma_theta*sigma_theta)
-            kernel[j, u] = 1/(2*np.pi*sigma_rho*sigma_theta*np.sqrt(1-r*r))*np.exp(-z0/(2*(1-r*r)))#+1/(2*np.pi*sigma_rho*sigma_theta*np.sqrt(1-r*r))*np.exp(-z1/(2*(1-r*r)))+1/(2*np.pi*sigma_rho*sigma_theta*np.sqrt(1-r*r))*np.exp(-z2/(2*(1-r*r)))
-    return kernel
 
 
+
+
+
+
+
+
+
+
+
+#def _kernel(angle, dist, sigma_rho, sigma_theta, sigma_rho_theta, rho, theta_y):
+#    """
+#    The Kernel function allows the user to compute the angle-distance kernel of the Hough Transform with the specified angle, distance and variances/covariances
+#    """
+#    angle = angle*np.pi/180.
+#    r = sigma_rho_theta/(sigma_rho*sigma_theta)
+#    kernel = np.zeros((dist.shape[0],angle.shape[0]))
+#    for u, i in enumerate(angle):
+#        for j, k in enumerate(dist):
+#            z0 = (k-rho)*(k-rho)/(sigma_rho*sigma_rho)-2*r*(k-rho)*(i-theta_y)/(sigma_rho*sigma_theta)+(i-theta_y)*(i-theta_y)/(sigma_theta*sigma_theta)
+##            z1 = (k+rho)*(k+rho)/(sigma_rho*sigma_rho)-2*r*(k+rho)*(i-theta_y-np.pi)/(sigma_rho*sigma_theta)+(i-theta_y-np.pi)*(i-theta_y-np.pi)/(sigma_theta*sigma_theta)
+##            z2 = (k+rho)*(k+rho)/(sigma_rho*sigma_rho)-2*r*(k+rho)*(i-theta_y+np.pi)/(sigma_rho*sigma_theta)+(i-theta_y+np.pi)*(i-theta_y+np.pi)/(sigma_theta*sigma_theta)
+#            kernel[j, u] = 1/(2*np.pi*sigma_rho*sigma_theta*np.sqrt(1-r*r))*np.exp(-z0/(2*(1-r*r)))#+1/(2*np.pi*sigma_rho*sigma_theta*np.sqrt(1-r*r))*np.exp(-z1/(2*(1-r*r)))+1/(2*np.pi*sigma_rho*sigma_theta*np.sqrt(1-r*r))*np.exp(-z2/(2*(1-r*r)))
+#    return kernel
+
+#def Segmentation(cluster):
+#    """
+#    This function is a failed attempt at duplicating the "segmentation of linked points" presented in the 3D object recognition article.
+#    
+#    The results do not work very well for our purpose.
+#    """
+#    p0, u ,v = u_v_(cluster)
+#    cp = _rotate(cluster, u, v, p0)
+#    index = np.argsort(cp[1])
+#    length, ratio = _ratio(cp)
+#    i = np.where(abs(cp[0, index])==max(abs(cp[0, index])))[0][0]
+#    cp0 = cp[:,i:]
+#    cp1 = cp[:,:i]
+#    if cp0.shape[1]<5 or cp1.shape[1]<5:
+#        ret = None
+#    else:
+#       clow = cluster.T[:,index[:i]].T
+#       chigh = cluster.T[:,index[i:]].T
+#       p0l, ul ,vl = u_v_(clow)
+#       p0h, uh ,vh = u_v_(chigh)
+#       cpl = _rotate(clow, ul, vl, p0l)
+#       cph = _rotate(chigh, uh, vh, p0h)
+#       lengthl, ratiol = _ratio(cpl)
+#       lengthh, ratioh = _ratio(cph)
+#       if ratiol>1.:
+#           ratiol=1./ratiol
+#       if ratioh>1.:
+#           ratioh=1./ratioh
+#       if ratiol<ratio or ratioh<ratio:
+#           ret = clow, chigh
+#       else:
+#           ret = None
+#   return ret
 
 
 
